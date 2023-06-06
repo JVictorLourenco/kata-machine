0f8f7b599540848a12f98d054c6d58a1bb9f14fe
João Victor <54009514+T4SUYA@users.noreply.github.com>
2023-06-06 11:17:59 -0300

    Implement recursive binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let hi = haystack.length;
+    let lo = 0;
+    let middle = Math.floor(lo + (hi - lo) / 2);
+    let v = haystack[middle];
 
+    if (lo >= hi) {
+        return false;
+    }
+
+    if (v === needle) {
+        return true;
+    } else if (v < needle) {
+        lo = middle + 1;
+    } else {
+        hi = middle;
+    }
+
+    return bs_list(haystack.slice(lo, hi), needle);
 }
