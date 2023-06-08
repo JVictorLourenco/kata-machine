3671c466ce400c620418b39b2b79ab322d9f3755
João Victor <54009514+T4SUYA@users.noreply.github.com>
2023-06-08 10:56:22 -0300

    Implement Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,38 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    public head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
-    push(item: T): void {
+    push(value: T): void {
+        const node = {
+            value: value,
+            prev: this.head,
+        } as Node<T>;
 
-}
+        this.length++;
+        this.head = node;
+    }
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        const value = this.head.value;
+        this.head = this.head.prev;
+        this.length--;
+
+        return value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
